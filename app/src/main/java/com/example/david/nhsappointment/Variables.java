9d0ce6595ef822b69e1616eae7e35262b1f66f9b
Jeremy <jjhotham@gmail.com>
2018-03-05 10:38:43 +0000

    variables file

---
+++ app/src/main/java/com/example/david/nhsappointment/Variables.java
@@ -0,0 +1,11 @@
+package com.example.david.nhsappointment;
+
+/**
+ * Created by Jeremy on 05/03/2018.
+ */
+
+public abstract class Variables {
+
+    public static String[] profile = {"Bob", "25/04/1991", "34 Huddersfield Road", "Dr. Greg Davies", "01421 893800", "bob@gmail.com"};
+
+}

f59385b28aade6132ad0f8afe13ea9727aaf3d76
Jeremy <jjhotham@gmail.com>
2018-03-05 10:46:27 +0000

    database update

--- app/src/main/java/com/example/david/nhsappointment/dataBaseConnection.java
+++ app/src/main/java/com/example/david/nhsappointment/dataBaseConnection.java
@@ -1,13 +1,5 @@
 package com.example.david.nhsappointment;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Properties;
-
 /**
  * Created by David on 05/02/2018.
  */

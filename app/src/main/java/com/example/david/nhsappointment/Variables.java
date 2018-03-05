11ce72c6fda919a9e5b608b19bf692c0f3aa5fe3
Jeremy <jjhotham@gmail.com>
2018-03-05 13:25:05 +0000

    variables

--- app/src/main/java/com/example/david/nhsappointment/Variables.java
+++ app/src/main/java/com/example/david/nhsappointment/Variables.java
@@ -8,5 +8,5 @@ public abstract class Variables {
 
     public static String[] profile = {"Bob", "25/04/1991", "34 Huddersfield Road", "Dr. Greg Davies", "01421 893800", "bob@gmail.com"};
     public static String[] login = {"bob@gmail.com", "25/04/1991"};
-
+    public static String[] symptoms = {};
 }
